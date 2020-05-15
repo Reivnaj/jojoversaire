fcd71ab535e86d9df51f82c4c5ac05ae52286b11
Reivnaj <pelletierjanvier@gmail.com>
2020-05-15 18:20:50 +0200

    anniv jojo

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,12 +4,12 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
 
-  validate :start_date_cannot_be_in_the_past
+  # validate :start_date_cannot_be_in_the_past
   validates :title, presence: true, length: { minimum: 5, maximum: 140}
   validates :description, presence: true, length: { minimum: 20, maximum: 1000}
-  validate :duration_must_be_multiple_of_five
-  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000}
-  validates :location, presence: true
+  #validate :duration_must_be_multiple_of_five
+  #validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000}
+  #validates :location, presence: true
 
   def start_date_cannot_be_in_the_past
     if start_date.present? && start_date < Date.today
